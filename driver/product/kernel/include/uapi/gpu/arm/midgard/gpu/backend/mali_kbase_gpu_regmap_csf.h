b8a9fd768478f318ab3df8e2f6c6ec59e4defb2c
Aryanxfa <aryan200152@gmail.com>
2022-12-27 11:25:08 +0530

    Update to r38p1

--- driver/product/kernel/include/uapi/gpu/arm/midgard/gpu/backend/mali_kbase_gpu_regmap_csf.h
+++ driver/product/kernel/include/uapi/gpu/arm/midgard/gpu/backend/mali_kbase_gpu_regmap_csf.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
  *
- * (C) COPYRIGHT 2019-2022 ARM Limited. All rights reserved.
+ * (C) COPYRIGHT 2022 ARM Limited. All rights reserved.
  *
  * This program is free software and is provided to you under the terms of the
  * GNU General Public License version 2 as published by the Free Software
@@ -19,18 +19,12 @@
  *
  */
 
-#ifndef _UAPI_KBASE_GPU_REGMAP_H_
-#define _UAPI_KBASE_GPU_REGMAP_H_
+#ifndef _UAPI_KBASE_GPU_REGMAP_CSF_H_
+#define _UAPI_KBASE_GPU_REGMAP_CSF_H_
 
-#if MALI_USE_CSF
-#include "backend/mali_kbase_gpu_regmap_csf.h"
-#else
-#include "backend/mali_kbase_gpu_regmap_jm.h"
-#endif /* !MALI_USE_CSF */
+/* IPA control registers */
+#define IPA_CONTROL_BASE 0x40000
+#define IPA_CONTROL_REG(r) (IPA_CONTROL_BASE + (r))
+#define STATUS 0x004 /* (RO) Status register */
 
-/* MMU control registers */
-#define MEMORY_MANAGEMENT_BASE  0x2000
-#define MMU_REG(r)              (MEMORY_MANAGEMENT_BASE + (r))
-#define MMU_IRQ_RAWSTAT         0x000   /* (RW) Raw interrupt status register */
-
-#endif /* _UAPI_KBASE_GPU_REGMAP_H_ */
+#endif /* _UAPI_KBASE_GPU_REGMAP_CSF_H_ */
